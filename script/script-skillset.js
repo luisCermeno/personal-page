0ad526be8fd567cdfbe92827dadc2b5303eff1e4
luisCermeno <luis.cermeno.f@gmail.com>
2022-03-09 14:18:40 -0800

    add dummy text img to skillset page

---
+++ script/script-skillset.js
@@ -0,0 +1,19 @@
+//GET ELEMENTS ON DOC LOADED
+document.addEventListener('DOMContentLoaded' , function(){
+  const title = document.getElementById("title")
+  const skill1 = document.getElementById("skill1")
+  const skill2 = document.getElementById("skill2")
+  const skill3 = document.getElementById("skill3")
+  const skill4 = document.getElementById("skill4")
+})
+
+
+//PARALLEX EFFECT
+window.addEventListener("scroll", function() {
+  let offset = window.scrollY;
+  console.log(offset)
+  skill1.style.backgroundPositionY = offset * 0.7 + "px";
+  skill2.style.backgroundPositionY = offset * 0.7 + "px";
+  skill3.style.backgroundPositionY = offset * 0.7 + "px";
+  skill4.style.backgroundPositionY = offset * 0.7 + "px";
+})
