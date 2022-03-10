328a036b722ac262c32b349c168c3266eb378f43
luisCermeno <luis.cermeno.f@gmail.com>
2022-03-10 10:16:46 -0800

    add opacity title

--- script/script-skillset.js
+++ script/script-skillset.js
@@ -1,17 +1,22 @@
 //GET ELEMENTS ON DOC LOADED
 document.addEventListener('DOMContentLoaded' , function(){
-  const title = document.getElementById("title")
+  // const title = document.getElementById("title")
   const skill1 = document.getElementById("skill1")
   const skill2 = document.getElementById("skill2")
   const skill3 = document.getElementById("skill3")
   const skill4 = document.getElementById("skill4")
+  const titleSection = document.getElementById("titleSection")
+  const title = document.getElementById("title");
 })
 
 
-//PARALLEX EFFECT
+// EFFECT
 window.addEventListener("scroll", function() {
   let offset = window.scrollY;
-  title.style.height = (400 - 3 * offset) + "px";
+  //title section effect
+  titleSection.style.height = (400 - 3 * offset) + "px";
+  title.style.opacity = 1 - 0.02 * offset;
+  //skill img effect
   skill1.style.backgroundPositionY = offset * 0.7 + "px";
   skill2.style.backgroundPositionY = offset * 0.7 + "px";
   skill3.style.backgroundPositionY = offset * 0.7 + "px";
